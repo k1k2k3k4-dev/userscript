6246080ab3d95c1fa1d79914b1e88044f4de86df
k1k2k3k4-dev <129705874+k1k2k3k4-dev@users.noreply.github.com>
2023-04-03 10:25:38 +0800

    Update license

--- 全盈支付-開放選擇付款 app.user.js
+++ 全盈支付-開放選擇付款 app.user.js
@@ -8,6 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pluspay.com.tw
 // @run-at       document-start
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function () {
