ef64e867a0efc696a5ae599c52033978a4463af8
k1k2k3k4-dev <129705874+k1k2k3k4-dev@users.noreply.github.com>
2023-04-04 17:09:40 +0800

    Update 全盈支付-開放選擇付款 app.user.js
    Add supporf for STAYFUN app.

--- 全盈支付-開放選擇付款 app.user.js
+++ 全盈支付-開放選擇付款 app.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         全盈支付-開放選擇付款 app
 // @namespace    https://github.com/k1k2k3k4-dev
-// @version      0.1
+// @version      0.2
 // @description  原本全盈支付網頁的開啟順序是先開啟全家 app，如果找不到全家 app，就會再開啟智生活 app，這導致智生活的用戶必須先卸載全家 app 才能順利開啟。這個 UserScript 可以解決這個問題，讓用戶可以自由選擇要使用哪一個 app 付款。
 // @author       k1k2k3k4-dev
 // @match        https://payment.pluspay.com.tw/payment/QRCode/*
@@ -34,8 +34,10 @@
         const fm = document.querySelector('.btn-box.bbx-btm .btn');
         fm.style.margin = '5px 0px';
         const smartdaily = fm.parentElement.appendChild(fm.cloneNode());
+        const stayfun = fm.parentElement.appendChild(fm.cloneNode());
         editElement(fm, 'fm', '全家');
         editElement(smartdaily, 'smartdaily', '智生活');
+        editElement(stayfun, 'stayfun', 'STAYFUN');
         window.callScheme = new Function('app',
             window.callScheme.toString()
                 .replace(/.*{([\s\S]*)}/, '$1') //拆解 callScheme function
