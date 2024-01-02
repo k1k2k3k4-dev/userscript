eac0c9925811e24ec63a0f620fae3d6d606d5aa5
k1k2k3k4-dev <129705874+k1k2k3k4-dev@users.noreply.github.com>
2024-01-03 00:06:38 +0800

    Update 全盈支付-開放選擇付款 app.user.js
    新增支援全盈+PAY 獨立 App
    Google Play https://play.google.com/store/apps/details?id=tw.com.pluspay.oneapp
    App Store https://apps.apple.com/tw/app/id6458691609

--- 全盈支付-開放選擇付款 app.user.js
+++ 全盈支付-開放選擇付款 app.user.js
@@ -35,9 +35,11 @@
         fm.style.margin = '5px 0px';
         const smartdaily = fm.parentElement.appendChild(fm.cloneNode());
         const stayfun = fm.parentElement.appendChild(fm.cloneNode());
+        const pp = fm.parentElement.appendChild(fm.cloneNode());
         editElement(fm, 'fm', '全家（或系統指定之 app）');
         editElement(smartdaily, 'smartdaily', '智生活');
         editElement(stayfun, 'stayfun', 'STAYFUN');
+        editElement(pp, 'pp', '全盈+PAY');
         window.callScheme = new Function('app',
             window.callScheme.toString()
                 .replace(/.*{([\s\S]*)}/, '$1') //拆解 callScheme function
